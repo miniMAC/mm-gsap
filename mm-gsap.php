d1bec43a262f7dd3b5267fc51213e334beac09d4
Christian Pucci <minimamente.info@gmail.com>
2024-05-07 15:15:24 +0200

    First commit

---
+++ mm-gsap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name:       MM
+ * Plugin URI:        https://minimamente.com
+ * Description:       Inclusione di custom CSS, JS e animazioni GSAP
+ * Version:           1.0.0
+ * Author:            miniMAC
+ * Author URI:        https://minimamente.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       mm
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
